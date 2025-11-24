6921467f689e361bfbd870082019f5c4470f7b4a
huuhoan229 <hhoan.cv@gmail.com>
2025-11-24 15:08:37 +0700

    3.8

---
+++ tamdung.bat
@@ -0,0 +1,9 @@
+@echo off
+echo DANG CHUYEN SANG CHE DO TAM DUNG...
+echo. > PAUSE_MODE
+echo =======================================
+echo DA TAT BOT THANH CONG!
+echo Bot se khong tra loi tin nhan nua.
+echo (Nhung Server van chay de Facebook khong khoa Webhook).
+echo =======================================
+timeout /t 5
---
+++ tieptuc.bat
@@ -0,0 +1,8 @@
+@echo off
+echo DANG KHOI DONG LAI BOT...
+del PAUSE_MODE
+echo =======================================
+echo DA BAT BOT THANH CONG!
+echo Bot se tra loi khach hang binh thuong.
+echo =======================================
+timeout /t 5
